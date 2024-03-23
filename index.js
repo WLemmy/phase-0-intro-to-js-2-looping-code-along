8739ff71117bc4aaa3b55d663907576e208bae0f
Wilfred Lemayian <wilfred.lemayian@gmail.com>
2024-03-23 21:21:28 +0300

    Looping Lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solutions in this file
+const countDown = (number) =>  {
+
+ if(number >= 0 || !number.isInteger) {
+console.log(" this is not a number");}
+
+for (let i = number; i--; i<=10) {
+ console.log(i);
+}}
+  countDown(10);
